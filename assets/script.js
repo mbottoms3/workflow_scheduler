6269f5d86799fd14b1ef79a2d5a6251c32c028fe
Micah Bottoms <bottoms.micah.3@gmail.com>
2022-10-03 21:53:01 -0600

    worked on local storage functionality

--- assets/script.js
+++ assets/script.js
@@ -4,26 +4,27 @@ var saveButton = document.querySelector(".saveBtn");
 var taskEntry = document.querySelector(".input");
 $("#currentDay").text(today.format("MMM Do, YYYY"));
 
-//finish finding a way to target the text user enters, then save it to local storage that persists through a page refresh
-var taskOutput = localStorage.getItem("taskEntry");
-
-taskEntry.textContent = taskOutput;
+// console.log(taskEntry);
 
 saveButton.addEventListener("click", function() {
     
-    localStorage.setItem("taskEntry", (taskEntry));
-    // renderMessage();
-    console.log(taskEntry);
+    localStorage.setItem("taskEntry", JSON.stringify(taskEntry.value));
+    
+    saveInput();
+    console.log(taskEntry.value);
 
 });
 
-// function renderMessage() {
-//     var key = true;
-//     var finalTask = JSON.parse(localStorage.getItem("taskEntry"));
+function saveInput() {
+    var taskOutput = localStorage.getItem("taskEntry");
+    var key = true;
+    if (key) {
+        taskOutput = JSON.parse(localStorage.getItem("taskEntry"));
+    }
+    // var newTask = JSON.stringify(taskOutput);
+    // console.log(newTask);
+    // localStorage.setItem("taskEntry", newTask);
+    return taskOutput;
 
-//     if (key) {
-//         taskEntry.textContent = finalTask;
-        
-//     }
-    
-// }
+   
+}
